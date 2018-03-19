9df14e974b91599b3339ebed23639739e3c849ba
shavenking <shavenking@gmail.com>
2018-03-19 17:13:46 +0800

    Update tintColor

--- switf-twitter-clone/AppDelegate.swift
+++ switf-twitter-clone/AppDelegate.swift
@@ -5,6 +5,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UINavigationBar.appearance().tintColor = UIColor(red: 67 / 255, green: 62 / 255, blue: 144 / 255, alpha: 1)
+        UITabBar.appearance().tintColor = UIColor(red: 67 / 255, green: 62 / 255, blue: 144 / 255, alpha: 1)
+        
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [
             UINavigationController(rootViewController: HomeViewController())
